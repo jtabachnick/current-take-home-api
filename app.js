991ff763ea4dd6658c92c4d65f25e8003565c224
jtabachnick <jeff.tabachnick@namely.com>
2020-05-10 20:35:03 -0400

    separating server

--- app.js
+++ app.js
@@ -5,46 +5,54 @@ const { getVisit, searchVisits, saveVisit } = require("./visits");
 const app = express();
 app.use(bodyParser.json());
 
+// get route
 app.get("/visit", async (req, res) => {
   let response;
   let statusCode = 200;
   try {
+    // if a visitId present call get visit to return single visit
     if (req.query.visitId) {
       response = await getVisit(req.query.visitId);
+
+      // if userId and searchString are present search the user's visits
     } else if (req.query.userId && req.query.searchString) {
       response = await searchVisits(req.query.userId, req.query.searchString);
+
+      // otherwise it's a bad request
     } else {
       statusCode = 400;
       response = "requsts must contain a visitId or userId and searchString";
     }
+    // catch any server errors and display, mostly for debugging
   } catch (e) {
     statusCode = 500;
     response = `we have encountered an error: ${e}`;
   } finally {
+    // send the response
     res.status(statusCode).send(response);
   }
 });
 
 app.post("/visit", async (req, res) => {
+  // validate that the body has needed fields respond with bad request if not
   if (!req.body.userId || !req.body.name) {
     res.status(400).send("userId and name (location) are required fields");
   }
   let response;
   let statusCode = 200;
 
+  // if fields are present save them
   try {
     response = await saveVisit(req.body.userId, req.body.name);
+
+    // catch and display server errors
   } catch (e) {
     statusCode = 500;
     response = `we have encountered an error: ${e}`;
   } finally {
+    // send response
     res.status(statusCode).send(response);
   }
 });
 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log("Press Ctrl+C to quit.");
-});
+module.exports = app;
--- visits.js
+++ visits.js
@@ -5,18 +5,31 @@ const { v4: uuid } = require("uuid");
 const datastore = new Datastore();
 
 const getVisit = async (visitId) => {
+  // create the db query of location entities
+  // filtering by visitId
   const query = datastore
     .createQuery("location")
     .filter("visitId", "=", visitId);
+
+  // running the query and waiting for the repsonse
   const data = await datastore.runQuery(query);
+
+  // cleaning out unneeded properties for response
   return cleanData(data[0]);
 };
 
 // exported functions
 const saveVisit = async (userId, location) => {
+  // database key needed for import
   const locationKey = datastore.key("location");
+
+  // unique identifier for individual gets
   const visitId = uuid();
+
+  // created date for recency sorting on search
   const created = Date.now();
+  
+  // creating entity
   const visit = {
     key: locationKey,
     data: {
@@ -27,37 +40,47 @@ const saveVisit = async (userId, location) => {
     },
   };
 
+  // saving entity
   await datastore.upsert(visit);
+
+  //return unique identifier
   return { visitId: visitId };
 };
 
 const searchVisits = async (userId, searchString) => {
+  // retrieve the visits by user before sorting and searching
   const data = await listVisits(userId);
+
+  // sort by most recent and return the 5 most recent
   let sortedData = sortVisits(data[0]);
 
+  // fuzzy search the sorted list
   const fuse = new Fuse(sortedData, {
     keys: ['name']
   });
-
   const searched = fuse.search(searchString);
-  const cleaned = cleanData(searched.map(s => s.item));
-  return cleaned;
+
+  // clean the searched list for response
+  return cleanData(searched.map(s => s.item));
 };
 
 
 // helpers 
 const cleanData = (visits) => {
+  // visit date is only used for sorting by date not needed in response
   visits.forEach((visit) => {
     delete visit.visitDate;
   });
   return visits;
 };
 
+// gets all visits by user
 const listVisits = (userId) => {
   const query = datastore.createQuery("location").filter("userId", "=", userId);
   return datastore.runQuery(query);
 };
 
+// sorts visits by date
 const sortVisits = (visits) => {
   let sortedData = visits.sort((a, b) => (a.visitDate > b.visitDate ? -1 : 1));
   return sortedData.length > 5 ? sortedData.slice(0,5) : sortedData;
